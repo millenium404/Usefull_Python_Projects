d4d28b7cb79b43ae854854582ff0a3feef59a071
milenski <alarak.bg@protonmail.com>
2022-11-23 11:43:39 +0200

    update

--- Tkinter_Tutorial/database_app.py
+++ Tkinter_Tutorial/database_app.py
@@ -1,5 +1,6 @@
 import os
 from tkinter import *
+import sqlite3
 
 
 root = Tk()
@@ -9,7 +10,55 @@ mheight = int(root.winfo_screenheight() // 1.3) # Get screen resolution
 root.geometry(f'{mwidth}x{mheight}') # Set main window size
 base_dir = os.path.abspath(os.getcwd())
 
+# Create Database or connect to one
+conn = sqlite3.connect('address_book.db')
+# Create Cursor
+c = conn.cursor()
 
+# Create Table
+# c.execute("""CREATE TABLE addresses (
+#         first_name text,
+#         last_name text,
+#         address text,
+#         city text,
+#         state text,
+#         zipcode integer)""")
 
+# Frame for registration
+frame_1 = LabelFrame(root, text='Register User', padx=50, pady=50)
+frame_1.grid(row=0, column=0, padx=10, pady=10)
+
+# Create Text Boxes
+f_name = Entry(frame_1, width=30)
+f_name.grid(row=0, column=1, padx=20, pady=5)
+l_name = Entry(frame_1, width=30)
+l_name.grid(row=1, column=1, padx=20, pady=5)
+address = Entry(frame_1, width=30)
+address.grid(row=2, column=1, padx=20, pady=5)
+city = Entry(frame_1, width=30)
+city.grid(row=3, column=1, padx=20, pady=5)
+state = Entry(frame_1, width=30)
+state.grid(row=4, column=1, padx=20, pady=5)
+zipcode = Entry(frame_1, width=30)
+zipcode.grid(row=5, column=1, padx=20, pady=5)
+
+# Create Text Box Labels
+f_name_label = Label(frame_1, text='First Name')
+f_name_label.grid(row=0, column=0)
+l_name_label = Label(frame_1, text='Last Name')
+l_name_label.grid(row=1, column=0)
+address_label = Label(frame_1, text='Address')
+address_label.grid(row=2, column=0)
+city_label = Label(frame_1, text='City')
+city_label.grid(row=3, column=0)
+state_label = Label(frame_1, text='State')
+state_label.grid(row=4, column=0)
+zipcode_label = Label(frame_1, text='Zip Code')
+zipcode_label.grid(row=5, column=0)
+
+# Commit Changes
+conn.commit()
+# Close connection
+conn.close()
 
 root.mainloop()
